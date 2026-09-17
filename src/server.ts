f316711cc559f90c9c19a947a7f2eb9527cb23ff
TaranaKhanna <taranakhanna5@gmail.com>
2026-09-17 16:18:31 +0000

    feat: integrate Prisma client in server shutdown process

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import { createApp } from "app.js";
 import { env } from "@config/env.js";
 import { logger } from "@config/logger.js";
+import { prisma } from "@db/prisma.js";
 
 const app = createApp();
 
@@ -8,21 +9,22 @@ const server = app.listen(env.port, () => {
     logger.info(`Server listening on port ${env.port} [${env.nodeEnv}]`);
 });
 
-// function shutdown(signal: string): void {
-//   logger.info(`${signal} received. Shutting down gracefully...`);
-//   server.close((err) => {
-//     if (err) {
-//       logger.error({ err }, 'Error during shutdown');
-//       process.exit(1);
-//     }
-//     logger.info('Server closed. Exiting.');
-//     process.exit(0);
-//   });
-// }
+function shutdown(signal: string): void {
+  logger.info(`${signal} received. Shutting down gracefully...`);
+  server.close(async (err) => {
+    if (err) {
+      logger.error({ err }, 'Error during shutdown');
+      process.exit(1);
+    }
+    await prisma.$disconnect();
+    logger.info('Server closed. Exiting.');
+    process.exit(0);
+  });
+}
 
-// process.on('SIGTERM', () => shutdown('SIGTERM'));
-// process.on('SIGINT', () => shutdown('SIGINT'));
+process.on('SIGTERM', () => shutdown('SIGTERM'));
+process.on('SIGINT', () => shutdown('SIGINT'));
 
-// process.on('unhandledRejection', (reason) => {
-//   logger.error({ reason }, 'Unhandled promise rejection');
-// });
+process.on('unhandledRejection', (reason) => {
+  logger.error({ reason }, 'Unhandled promise rejection');
+});
